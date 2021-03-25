6626778eab456e8bdfa5068352ca57f5f654f9ef
shejialuo <shejialuo@gmail.com>
2021-03-25 17:11:32 +0800

    First Commit

---
+++ DataBase/DataBase.hpp
@@ -0,0 +1,9 @@
+#ifndef DATABASE_HPP
+#define DATABASE_HPP
+
+class DATABASE {
+private:
+    int numberOfMessageInTheMonitorWindow;
+};
+
+#endif // DATABASE_HPP
---
+++ MailGenerator/MailGenerator.hpp
@@ -0,0 +1,12 @@
+#ifndef MAILGENERATOR_HPP
+#define MAILGENERATOR_HPP
+
+class MailGenerator {
+private:
+    static int 
+public:
+    void run();
+};
+
+
+#endif // MAILGENERATOR_HPP
